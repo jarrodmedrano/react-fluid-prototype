92f13e979038d7383bc17a66ac348c0b16122a86
Jarrod Medrano (Insight Global) <v-jamedr@microsoft.com>
2015-11-11 12:09:43 -0800

    adding browsersync

--- RDX.SW/gulpfile.js
+++ RDX.SW/gulpfile.js
@@ -4,8 +4,20 @@ var gulp = require('gulp'),
     cssmin = require('gulp-cssmin'),
     concat = require("gulp-concat"),
     rename = require('gulp-rename');
+    browserSync = require('browser-sync').create();
 
-gulp.task('watch', function () {
+// use default task to launch Browsersync and watch JS files
+gulp.task('serve', ['less'], function () {
+
+    // Serve files from the root of this project
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    // add browserSync.reload to the tasks array to make
+    // all browsers reload after tasks are complete.
     gulp.watch('src/less/**/*.less', ['less']);
 });
 
