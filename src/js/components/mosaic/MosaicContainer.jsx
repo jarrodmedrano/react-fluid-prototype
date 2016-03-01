75710823cfc91e9df42ddb8bac77b7d95ed2e5df
Jarrod Medrano <jmedran@gmail.com>
2016-03-01 15:55:13 -0800

    obfuscate mosaic data

--- src/js/components/mosaic/MosaicContainer.jsx
+++ src/js/components/mosaic/MosaicContainer.jsx
@@ -2,14 +2,40 @@ import React from 'react';
 import Mosaic from 'src/js/components/mosaic/mosaic.jsx!';
 
 var MosaicContainer = React.createClass({
+
+
     render() {
+
+        let defaultMosaic = [
+            {
+                mosaicTitle: "Rise of the Tomb Raider",
+                mosaicSize: "f-vp1-whole f-vp2-half f-vp3-quarter f-height-medium",
+                mosaicImage: "http://www.getmwf.com/images/components/placement-background-tombraider.jpg"
+            },
+            {
+                mosaicTitle: "Forza Horizon 2",
+                mosaicSize: "f-vp1-whole f-vp2-half f-vp3-quarter f-height-medium",
+                mosaicImage: "http://www.getmwf.com/images/components/placement-background-forza.jpg"
+            },
+            {
+                mosaicTitle: "Xbox One Elite bundle",
+                mosaicSize: "f-vp1-whole f-vp2-half f-vp3-quarter f-height-medium",
+                mosaicImage: "http://www.getmwf.com/images/components/placement-background-xboxcontroller.jpg"
+            },
+            {
+                mosaicTitle: "Halo 5",
+                mosaicSize: "f-vp1-whole f-vp2-half f-vp3-quarter f-height-medium",
+                mosaicImage: "http://www.getmwf.com/images/components/placement-background-halo.jpg"
+            }
+        ];
+
         return (
             <div className="c-mosaic">
                 <div data-f-mosaic={this.props.containerSize}>
-                    <Mosaic mosaicTitle="Rise of the Tomb Raider" mosaicImage="http://www.getmwf.com/images/components/placement-background-tombraider.jpg" mosaicSize="f-vp1-whole f-vp2-half f-vp3-quarter f-height-medium" />
-                    <Mosaic mosaicTitle="Forza Horizon 2" mosaicImage="http://www.getmwf.com/images/components/placement-background-forza.jpg" mosaicSize="f-vp1-whole f-vp2-half f-vp3-quarter f-height-medium" />
-                    <Mosaic mosaicTitle="Xbox One Elite bundle" mosaicImage="http://www.getmwf.com/images/components/placement-background-xboxcontroller.jpg" mosaicSize="f-vp1-whole f-vp2-half f-vp3-quarter f-height-medium" />
-                    <Mosaic mosaicTitle="Halo 5" mosaicImage="http://www.getmwf.com/images/components/placement-background-halo.jpg" mosaicSize="f-vp1-whole f-vp2-half f-vp3-quarter f-height-medium" />
+                    <Mosaic mosaic={defaultMosaic[0]} />
+                    <Mosaic mosaic={defaultMosaic[1]} />
+                    <Mosaic mosaic={defaultMosaic[2]} />
+                    <Mosaic mosaic={defaultMosaic[3]} />
                 </div>
             </div>
         )
--- src/js/components/mosaic/mosaic.jsx
+++ src/js/components/mosaic/mosaic.jsx
@@ -4,18 +4,19 @@ import 'src/js/components/mosaic/mosaic.scss!';
 var Mosaic = React.createClass({
 
     render() {
+
         return(
-            <div className="theme-dark" data-f-mosaic={this.props.mosaicSize}>
+            <div className="theme-dark" data-f-mosaic={this.props.mosaic.mosaicSize}>
                 <article className="c-placement context-accessory f-width-small f-height-large">
                     <picture>
-                        <img srcSet={this.props.mosaicImage} src={this.props.mosaicImage} alt={this.props.mosaicTitle} />
+                        <img srcSet={this.props.mosaic.mosaicImage} src={this.props.mosaic.mosaicImage} alt={this.props.mosaic.mosaicTitle} />
                     </picture>
 
                     <div className="c-image-overlay" aria-hidden="true"></div>
                     <div>
                         <dl>
                         <dt className="x-screen-reader">Game Title</dt>
-                        <dd><cite>{this.props.mosaicTitle}</cite></dd>
+                        <dd><cite>{this.props.mosaic.mosaicTitle}</cite></dd>
                         <div className="c-group">
                             <a href="/components/items/mosaic.html" className="c-call-to-action c-glyph"><span>Buy now</span></a>
                         </div>
