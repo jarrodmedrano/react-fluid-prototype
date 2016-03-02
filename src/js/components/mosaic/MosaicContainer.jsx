d2b20b285d9c77465fb52919c91078269f6329cb
Jarrod Medrano <jmedran@gmail.com>
2016-03-01 16:05:24 -0800

    refactoring mosaic container

--- src/js/components/mosaic/MosaicContainer.jsx
+++ src/js/components/mosaic/MosaicContainer.jsx
@@ -6,36 +6,14 @@ var MosaicContainer = React.createClass({
 
     render() {
 
-        let defaultMosaic = [
-            {
-                mosaicTitle: "Rise of the Tomb Raider",
-                mosaicSize: "f-vp1-whole f-vp2-half f-vp3-quarter f-height-medium",
-                mosaicImage: "http://www.getmwf.com/images/components/placement-background-tombraider.jpg"
-            },
-            {
-                mosaicTitle: "Forza Horizon 2",
-                mosaicSize: "f-vp1-whole f-vp2-half f-vp3-quarter f-height-medium",
-                mosaicImage: "http://www.getmwf.com/images/components/placement-background-forza.jpg"
-            },
-            {
-                mosaicTitle: "Xbox One Elite bundle",
-                mosaicSize: "f-vp1-whole f-vp2-half f-vp3-quarter f-height-medium",
-                mosaicImage: "http://www.getmwf.com/images/components/placement-background-xboxcontroller.jpg"
-            },
-            {
-                mosaicTitle: "Halo 5",
-                mosaicSize: "f-vp1-whole f-vp2-half f-vp3-quarter f-height-medium",
-                mosaicImage: "http://www.getmwf.com/images/components/placement-background-halo.jpg"
-            }
-        ];
+        let results = this.props.mosaic;
 
         return (
             <div className="c-mosaic">
                 <div data-f-mosaic={this.props.containerSize}>
-                    <Mosaic mosaic={defaultMosaic[0]} />
-                    <Mosaic mosaic={defaultMosaic[1]} />
-                    <Mosaic mosaic={defaultMosaic[2]} />
-                    <Mosaic mosaic={defaultMosaic[3]} />
+                    {results.map(function(result, id) {
+                        return  <Mosaic key={id} mosaic={results[id]} />;
+                    })}
                 </div>
             </div>
         )
--- src/js/components/pages/HomePage.jsx
+++ src/js/components/pages/HomePage.jsx
@@ -7,10 +7,33 @@ import Carousel from 'src/js/components/carousel/carousel.jsx!';
 
 var HomePage = React.createClass({
     render() {
+        let defaultMosaic = [
+            {
+                mosaicTitle: "Rise of the Tomb Raider",
+                mosaicSize: "f-vp1-whole f-vp2-half f-vp3-quarter f-height-medium",
+                mosaicImage: "http://www.getmwf.com/images/components/placement-background-tombraider.jpg"
+            },
+            {
+                mosaicTitle: "Forza Horizon 2",
+                mosaicSize: "f-vp1-whole f-vp2-half f-vp3-quarter f-height-medium",
+                mosaicImage: "http://www.getmwf.com/images/components/placement-background-forza.jpg"
+            },
+            {
+                mosaicTitle: "Xbox One Elite bundle",
+                mosaicSize: "f-vp1-whole f-vp2-half f-vp3-quarter f-height-medium",
+                mosaicImage: "http://www.getmwf.com/images/components/placement-background-xboxcontroller.jpg"
+            },
+            {
+                mosaicTitle: "Halo 5",
+                mosaicSize: "f-vp1-whole f-vp2-half f-vp3-quarter f-height-medium",
+                mosaicImage: "http://www.getmwf.com/images/components/placement-background-halo.jpg"
+            }
+        ];
+
         return (
             <div>
                 <Carousel />
-                <MosaicContainer containerSize="f-vp1-whole f-vp4-whole" />
+                <MosaicContainer mosaic={defaultMosaic} containerSize="f-vp1-whole f-vp4-whole" />
             </div>
         );
     }
