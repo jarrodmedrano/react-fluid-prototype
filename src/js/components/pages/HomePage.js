74de46453e824d0f9bff2fe86795bcc863be53cf
Jarrod Medrano <jmedran@gmail.com>
2016-03-03 13:19:06 -0800

    working flexbox layout

--- src/js/components/pages/HomePage.js
+++ src/js/components/pages/HomePage.js
@@ -7,30 +7,30 @@ var HomePage = React.createClass({
         let defaultMosaic = [
             {
                 mosaicTitle: "Rise of the Tomb Raider",
-                mosaicSize: "f-vp1-whole f-vp2-half f-vp3-quarter f-height-medium",
+                mosaicSize: "f-vp1-whole f-vp2-half f-vp3-quarter",
                 mosaicImage: "http://www.getmwf.com/images/components/placement-background-tombraider.jpg"
             },
             {
                 mosaicTitle: "Forza Horizon 2",
-                mosaicSize: "f-vp1-whole f-vp2-half f-vp3-quarter f-height-medium",
+                mosaicSize: "f-vp1-whole f-vp2-half f-vp3-quarter",
                 mosaicImage: "http://www.getmwf.com/images/components/placement-background-forza.jpg"
             },
             {
                 mosaicTitle: "Xbox One Elite bundle",
-                mosaicSize: "f-vp1-whole f-vp2-half f-vp3-quarter f-height-medium",
+                mosaicSize: "f-vp1-whole f-vp2-half f-vp3-quarter",
                 mosaicImage: "http://www.getmwf.com/images/components/placement-background-xboxcontroller.jpg"
             },
             {
                 mosaicTitle: "Halo 5",
-                mosaicSize: "f-vp1-whole f-vp2-half f-vp3-quarter f-height-medium",
+                mosaicSize: "f-vp1-whole f-vp2-half f-vp3-quarter",
                 mosaicImage: "http://www.getmwf.com/images/components/placement-background-halo.jpg"
             }
         ];
 
         return (
             <div>
-                <Hero heroTitle="What are you using your Surface for?" heroSrc="Surface_Book_Laptop_01" heroFileType="png" heroSize="f-medium" heroStyle={{'height': '30vh'}} />
-                <div className="c-mosaic" style={{'height': '70vh'}}>
+                <Hero heroTitle="What are you using your Surface for?" heroSrc="Surface_Book_Laptop_01" heroFileType="png" heroSize="f-medium"  />
+                <div className="c-mosaic">
                     <MosaicContainer mosaic={defaultMosaic} containerSize="f-vp1-whole f-vp4-whole" />
                 </div>
             </div>
