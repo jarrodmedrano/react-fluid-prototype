fb99b57a78d2c9db7c91ec266afb151f239b15aa
Jarrod Medrano <jmedran@gmail.com>
2016-03-16 12:58:01 -0700

    taking img path out of hero

--- src/js/components/hero/HeroPicture.js
+++ src/js/components/hero/HeroPicture.js
@@ -7,21 +7,21 @@ var HeroPicture = React.createClass({
         return (
             <picture>
                 <source
-                    srcSet={`img/${this.props.heroSrc}-vp5.${this.props.heroFileType}`}
+                    srcSet={`${this.props.heroSrc}-vp5.${this.props.heroFileType}`}
                     media="(min-width:1084px)"/>
                 <source
-                    srcSet={`img/${this.props.heroSrc}-vp4.${this.props.heroFileType}`}
+                    srcSet={`${this.props.heroSrc}-vp4.${this.props.heroFileType}`}
                     media="(min-width:768px)"/>
                 <source
-                    srcSet={`img/${this.props.heroSrc}-vp3.${this.props.heroFileType}`}
+                    srcSet={`${this.props.heroSrc}-vp3.${this.props.heroFileType}`}
                     media="(min-width:540px)"/>
                 <source
-                    srcSet={`img/${this.props.heroSrc}-vp2.${this.props.heroFileType}`}
+                    srcSet={`${this.props.heroSrc}-vp2.${this.props.heroFileType}`}
                     media="(min-width:0)"/>
 
                 <img
-                    srcSet={`img/${this.props.heroSrc}-vp4.${this.props.heroFileType}`}
-                    src={`img/${this.props.heroSrc}-vp4.${this.props.heroFileType}`}
+                    srcSet={`${this.props.heroSrc}-vp4.${this.props.heroFileType}`}
+                    src={`${this.props.heroSrc}-vp4.${this.props.heroFileType}`}
                     alt="Martian poster"/>
             </picture>
         )
