c4f72faa83d14a5256750ad8b3c6d206e5ebeb09
jmedran <jmedran@gmail.com>
2016-08-26 14:57:28 -0700

    remove build

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function (grunt) {
+grunt.initConfig({
+    buildcontrol: {
+            options: {
+                dir: 'dist',
+                commit: true,
+                push: true,
+                message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+            },
+            sitebox: {
+                options: {
+                    remote: 'https://v-jamedr@rdx-vertical.scm.sitebox-s.redant.selfhost.corp.microsoft.com:443/RDX-VERTICAL.git',
+                    branch: 'master'
+                }
+            }
+        }
+});
+
+grunt.loadNpmTasks('grunt-build-control');
+
+grunt.registerTask('deploy', ['buildcontrol:sitebox']);
+
+};
