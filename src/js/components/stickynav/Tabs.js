83d793877c256e89a20ea3e2b05ccb8f71bd6754
jmedran <jmedran@gmail.com>
2016-09-22 16:49:09 -0700

    tabs editing

--- src/js/components/stickynav/Tabs.js
+++ src/js/components/stickynav/Tabs.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './tabs.scss'
+import _ from 'lodash'
 import classNames from 'classnames'
 
 import { Router, Route, Link, IndexLink, IndexRoute } from 'react-router';
@@ -15,25 +16,36 @@ class Tabs extends React.Component {
 
         };
 
-        var logoTabs = this.props.groups.map(function(result) {
+        let brands = _.chain(this.props.groups).filter(function(result) {
+                return result.brand
+        }).map(function(result) {
+            return {
+                ['brand' + index]: result
+            }
+        }).value();
 
-           return result.brand.logoTab
 
-        }).reduce(function(result, item, i) {
+        // var logoTabs = brands.map(function(result, index) {
+        //
+        //    return  result
+        // });
 
-            result[i] = require(`img/${item}`);
-            return result;
 
-        }, {});
+        //     .reduce(function(result, item, i) {
+        //     console.log(item);
+        //
+        //     result[i] = require(`img/${item}`);
+        //     return result;
+        //
+        // }, {});
 
-        this.state = Object.assign(this.state, {logoTabs});
+        this.state = Object.assign(this.state, brands);
 
+        console.log(this.state);
 
     }
 
     render() {
-
-
 
         const depth = this.props.routes.length;
 
@@ -48,14 +60,14 @@ class Tabs extends React.Component {
         return (
             <div className="tabs">
                 <ul>
-                    <li className="c-hyperlink"><IndexLink to={rootRoute.path} activeClassName="active"><img alt={rootRoute.title} src={this.state.logoTabs[0]} /></IndexLink></li>
+                    <li className="c-hyperlink"><IndexLink to={rootRoute.path} activeClassName="active"><img alt={rootRoute.title}  /></IndexLink></li>
                     {rootRouteChildren !=null ? rootRouteChildren.map((item, index) =>
                         <li className="c-hyperlink" key={index}>
                             <Link
                                 activeClassName="active"
                                 activeStyle={{ background: 'red' }}
                                 to={item.path || ''}>
-                                <img src={this.state.logoTabs[index + 1]} alt={item.title} />
+                                <img  alt={item.title} />
                             </Link>
                             {(index + 1) < depth}
                         </li>
