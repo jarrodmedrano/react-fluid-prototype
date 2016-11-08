c4a42fec0619d01f01fa8c401c9db1ad23091b31
jmedran <jmedran@gmail.com>
2016-11-08 01:51:25 -0800

    adding validateProp and createChainable

--- src/js/components/mosaic/MosaicTile.js
+++ src/js/components/mosaic/MosaicTile.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import Picture from '../picture/Picture';
 import Heading from '../heading/Heading';
-import propsAreValid from '../../util';
+import validateProp from '../../util';
 
 class MosaicTile extends React.Component {
     render() {
-        if(propsAreValid(this.props)) {
             return (
             <section className="c-mosaic-placement">
                 <Picture data={this.props.data} />
                 <Heading data={this.props.data} />
             </section>
-        )} else {
-            return null
-        }
+            )
     }
 }
 
 export default MosaicTile;
+
+MosaicTile.propTypes = {
+    data: validateProp
+};
--- src/js/util.js
+++ src/js/util.js
@@ -1,22 +1,63 @@
 import _ from 'lodash';
 
-function validateProps(props) {
-    _.filter(props, function(o) { console.log(o.isRequired) });
+export function createChainableTypeChecker(validate) {
+    function checkType(isRequired, props, propName, componentName, location) {
+        componentName = componentName || ANONYMOUS;
+        if (props[propName] == null) {
+            var locationName = ReactPropTypeLocationNames[location];
+            if (isRequired) {
+                return new Error(
+                    ("Required " + locationName + " `" + propName + "` was not specified in ") +
+                    ("`" + componentName + "`.")
+                );
+            }
+            return null;
+        } else {
+            return validate(props, propName, componentName, location);
+        }
+    }
+
+    var chainedCheckType = checkType.bind(null, false);
+    chainedCheckType.isRequired = checkType.bind(null, true);
+
+    return chainedCheckType;
 }
 
-export default function propsAreValid(props) {
-    validateProps(props);
-    if(!props.data) {
-       return false
-    } else {
-        return true
+export default function validateProp (props, propName, componentName, location) {
+    componentName = componentName || 'ANONYMOUS';
+
+    if (!props[propName]) {
+       return new Error(componentName + ' is missing propType ' + propName + '.');
     }
+
+    return null;
 }
 
-export function toggleRender(props, component) {
-    if(!props.data) {
-        return false
-    } else {
-        return component
-    }
-}
+//
+// function validateProps(props) {
+//     _.filter(props, function(o) { console.log(o.isRequired) });
+// }
+//
+// export default function propsAreValid(props, proptypes) {
+//    let myProps = _.reduce(props, function(newVal, previousVal, key) {
+//
+//
+//        return previousVal
+//    });
+//
+//     console.log(myProps);
+//
+//     if(!props.data) {
+//        return false
+//     } else {
+//         return true
+//     }
+// }
+//
+// export function toggleRender(props, component) {
+//     if(!props.data) {
+//         return false
+//     } else {
+//         return component
+//     }
+// }
