4c44d8672d6c25c3ffc97f161baf58d34e3acb9f
Jarrod Medrano (Insight Global) <v-jamedr@microsoft.com>
2016-11-08 10:24:25 -0800

    fixing broken mosaictile

--- src/js/components/mosaic/MosaicTile.js
+++ src/js/components/mosaic/MosaicTile.js
@@ -3,27 +3,13 @@ import Picture from '../picture/Picture';
 import Heading from '../heading/Heading';
 
 class MosaicTile extends React.Component {
-
-
-    componentWillMount() {
-        if(!this.props.data) {
-            this.state.render = false;
-        }
-    }
-
     render() {
-            if(this.state.render) {
-            return (
+        return (
             <section className="c-mosaic-placement">
-                <Picture data={this.props.data} />
-                <Heading data={this.props.data} />
+                <Picture data={this.props.data}/>
+                <Heading data={this.props.data}/>
             </section>
-            )
-            } else {
-                return null
-            }
+        )
     }
 }
-
-
 export default MosaicTile;
