6370ca582a02c57c80347f0b4ac4d2b711d9f2fc
jmedran <jmedran@gmail.com>
2016-11-27 22:55:36 -0800

    modify compare table

--- src/js/components/compare/CompareTable.js
+++ src/js/components/compare/CompareTable.js
@@ -8,29 +8,30 @@ import propsAreValid from '../../util';
 
 class CompareTable extends React.Component {
     render() {
-        if(propsAreValid(this.props.data)) {
+        if (propsAreValid(this.props.data)) {
             return (
                 <div>
                     <div className="config-title">
-                        <Heading data={this.props.data} />
+                        <Heading data={this.props.data}/>
                     </div>
                     <div className="configs">
-                        <div className="config-card text-center">
-                            <div className="config-top">
-                                <GenericList data={this.props.data} />
-                            </div>
-                            <div className="config-wrapper">
-                                {this.props.data.compare.models.map(function(result, id) {
-                                    return (
-                                        <CompareRow key={id} data={result.props} />
-                                    )
-                                })}
-                            </div>
-                        </div>
+                        {this.props.data.compare.models.map(function (result, id) {
+                            return (
+                                <div className="config-card text-center">
+                                    <div className="config-top">
+                                        <GenericList key={id} data={result.props}/>
+                                    </div>
+                                    <div className="config-wrapper">
+                                        <CompareRow key={id} data={result.props}/>
+                                    </div>
+                                </div>
+                            )
+                        })}
                     </div>
                 </div>
             )
-        } return null
+        }
+        return null
     }
 }
 
