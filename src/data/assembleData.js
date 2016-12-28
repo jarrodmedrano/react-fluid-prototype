19c3acbdf55381adc18cc33eaa6bf8ddede599ae
Jarrod Medrano (Insight Global) <v-jamedr@microsoft.com>
2016-12-28 13:58:49 -0800

    adding immersive hero data

--- src/data/assembleData.js
+++ src/data/assembleData.js
@@ -1,8 +1,9 @@
 import dummyData from './hubRoot.json!';
 import dummyHero from './dummyHero.json!';
 import dummyMosaic from './dummyMosaic.json!';
+import dummyIHero from './dummyIHero.json!';
 
-dummyData.groups.push(dummyHero, dummyMosaic);
+dummyData.groups.push(dummyHero, dummyMosaic, dummyIHero);
 let data = dummyData;
 export default data;
 
