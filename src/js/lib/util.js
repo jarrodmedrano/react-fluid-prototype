ea91ee0e862b6cfbd9c89721dd4b60ad56a94c0e
Jarrod Medrano (Insight Global) <v-jamedr@microsoft.com>
2017-02-07 20:22:40 +0000

    Merged PR 441: remove imported css
    remove imported css

--- src/js/lib/util.js
+++ src/js/lib/util.js
@@ -5,7 +5,7 @@ export function onClickHandler(e) {
     }
 }
 
-export default function propsAreValid(props, required) {
+export default function propsAreValid(props) {
     if(!props) {
         if(window.RDX) {
             window.RDX.logError('Invalid prop' + props);
@@ -19,42 +19,26 @@ export default function propsAreValid(props, required) {
 }
 
 
-//
-// let propsAreValid = createChainableTypeChecker(tweetLengthChecker);
-//
-// function tweetLengthChecker(props, propName, componentName, location) {
-//     componentName = componentName || 'ANONYMOUS';
-//
-//     if (props[propName]) {
-//         let value = props[propName];
-//         if (typeof value === 'string') {
-//             return value.length <= 140 ? null : new Error(propName + ' in ' + componentName + " is longer than 140 characters");
-//         }
-//     }
-//
-//     // assume all ok
-//     return null;
-// }
-//
-// function createChainableTypeChecker(validate) {
-//     function checkType(isRequired, props, propName, componentName, location) {
-//         componentName = componentName || ANONYMOUS;
-//         if (props[propName] == null) {
-//             var locationName = ReactPropTypeLocationNames[location];
-//             if (isRequired) {
-//                 return new Error(
-//                     ("Required " + locationName + " `" + propName + "` was not specified in ") +
-//                     ("`" + componentName + "`.")
-//                 );
-//             }
-//             return null;
-//         } else {
-//             return validate(props, propName, componentName, location);
-//         }
-//     }
-//
-//     let chainedCheckType = checkType.bind(null, false);
-//     chainedCheckType.isRequired = checkType.bind(null, true);
-//
-//     return chainedCheckType;
-// }
+
+function createChainableTypeChecker(validate) {
+    function checkType(isRequired, props, propName, componentName, location) {
+        componentName = componentName || ANONYMOUS;
+        if (props[propName] == null) {
+            var locationName = ReactPropTypeLocationNames[location];
+            if (isRequired) {
+                return new Error(
+                    ("Required " + locationName + " `" + propName + "` was not specified in ") +
+                    ("`" + componentName + "`.")
+                );
+            }
+            return null;
+        } else {
+            return validate(props, propName, componentName, location);
+        }
+    }
+
+    let chainedCheckType = checkType.bind(null, false);
+    chainedCheckType.isRequired = checkType.bind(null, true);
+
+    return chainedCheckType;
+}
