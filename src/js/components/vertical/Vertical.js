707d83db9db6d303f19f99ecbca4b0732aaa6f3b
Jarrod Medrano (Insight Global) <v-jamedr@microsoft.com>
2017-03-03 00:37:13 +0000

    Merged PR 496: don't show unacceptable layouts
    don't show unacceptable layouts

--- src/js/components/vertical/Vertical.js
+++ src/js/components/vertical/Vertical.js
@@ -9,6 +9,7 @@ import LegacyKSP from '../legacy/legacyKsp';
 import LegacyCenteredBackdrop from '../legacy/legacycenteredbackdrop';
 import propsAreValid from '../../lib/util';
 import dataPropTypes, {verticalPropTypes} from '../../../data/dataProps';
+import _ from 'lodash';
 
 class Vertical extends React.Component {
     
@@ -35,31 +36,49 @@ class Vertical extends React.Component {
             let active = this.state.active ? 'active' : 'inactive';
             let verticalClass = classNames('scene-vertical', this.props.data.groupIdentifier, this.props.data.sectionIdentifier, active);
             let {layout, sectionIdentifier, readingDirection} = this.props.data;
-            return (
-                <section id={sectionIdentifier} className={verticalClass} name={sectionIdentifier} dir={readingDirection ? readingDirection : null}>
-                    {layout == 'hero' || layout == 'immersive-hero' || layout == 'fullscreen' || layout == 'card' ?
-                        <Hero data={this.props.data} brandColor={this.props.brandColor ? this.props.brandColor : null} active={this.state.active} updated={this.state.updated} /> : null
-                    }
-                    {layout == 'mosaic' ?
-                        <Mosaic data={this.props.data} brandColor={this.props.brandColor ? this.props.brandColor : null} active={this.state.active} updated={this.state.updated} /> : null
-                    }
-                    {layout == 'compare' ?
-                        <CompareTable data={this.props.data} brandColor={this.props.brandColor ? this.props.brandColor : null} active={this.state.active}  updated={this.state.updated} /> : null
-                    }
-                    {layout == 'feature' ?
-                        <LegacyFeature data={this.props.data} brandColor={this.props.brandColor ? this.props.brandColor : null} active={this.state.active} updated={this.state.updated} /> : null
-                    }
-                    {layout == 'featureCta' ?
-                        <LegacyFeature data={this.props.data} brandColor={this.props.brandColor ? this.props.brandColor : null} active={this.state.active} updated={this.state.updated} /> : null
-                    }
-                    {layout == 'ksp' || layout == 'ksp_rs' || layout == 'ksp_reversed' ?
-                        <LegacyKSP data={this.props.data}  brandColor={this.props.brandColor ? this.props.brandColor : null} active={this.state.active} updated={this.state.updated} />  : null
-                    }
-                    {layout == 'centeredBackdropTemplate' ?
-                        <LegacyCenteredBackdrop data={this.props.data} brandColor={this.props.brandColor ? this.props.brandColor : null} active={this.state.active} updated={this.state.updated} /> : null
-                    }
-                </section>
-            )
+            let acceptedLayouts = ['hero', 'immersive-hero', 'fullscreen', 'card', 'mosaic', 'compare', 'feature', 'featureCta', 'ksp', 'ksp_rs', 'ksp_reversed', 'centeredBackdropTemplate'];
+            if(layout && _.includes(acceptedLayouts, layout)) {
+                return (
+                    <section id={sectionIdentifier} className={verticalClass} name={sectionIdentifier}
+                             dir={readingDirection ? readingDirection : null}>
+                        {layout == 'hero' || layout == 'immersive-hero' || layout == 'fullscreen' || layout == 'card' ?
+                            <Hero data={this.props.data}
+                                  brandColor={this.props.brandColor ? this.props.brandColor : null}
+                                  active={this.state.active} updated={this.state.updated}/> : null
+                        }
+                        {layout == 'mosaic' ?
+                            <Mosaic data={this.props.data}
+                                    brandColor={this.props.brandColor ? this.props.brandColor : null}
+                                    active={this.state.active} updated={this.state.updated}/> : null
+                        }
+                        {layout == 'compare' ?
+                            <CompareTable data={this.props.data}
+                                          brandColor={this.props.brandColor ? this.props.brandColor : null}
+                                          active={this.state.active} updated={this.state.updated}/> : null
+                        }
+                        {layout == 'feature' ?
+                            <LegacyFeature data={this.props.data}
+                                           brandColor={this.props.brandColor ? this.props.brandColor : null}
+                                           active={this.state.active} updated={this.state.updated}/> : null
+                        }
+                        {layout == 'featureCta' ?
+                            <LegacyFeature data={this.props.data}
+                                           brandColor={this.props.brandColor ? this.props.brandColor : null}
+                                           active={this.state.active} updated={this.state.updated}/> : null
+                        }
+                        {layout == 'ksp' || layout == 'ksp_rs' || layout == 'ksp_reversed' ?
+                            <LegacyKSP data={this.props.data}
+                                       brandColor={this.props.brandColor ? this.props.brandColor : null}
+                                       active={this.state.active} updated={this.state.updated}/> : null
+                        }
+                        {layout == 'centeredBackdropTemplate' ?
+                            <LegacyCenteredBackdrop data={this.props.data}
+                                                    brandColor={this.props.brandColor ? this.props.brandColor : null}
+                                                    active={this.state.active} updated={this.state.updated}/> : null
+                        }
+                    </section>
+                )
+            }
         } return null
     }
 }
