e0fff5139cd62d35f1f160c1bfe166d68506e721
Jarrod Medrano (Insight Global) <v-jamedr@microsoft.com>
2017-04-19 21:07:19 +0000

    Merged PR 563: adding test for utility functions
    adding test for utility functions

---
+++ src/js/lib/test/util.test.js
@@ -0,0 +1,52 @@
+import {impressionEvent, externalNavigate, internalNavigate, logError, navigateEvent} from '../util';
+import expect, { createSpy, spyOn, isSpy } from 'expect'
+
+describe('Utility functions', () => {
+    it('should run external navigate if window.RDX exists', () => {
+        window.RDX = {
+            externalNavigate: () => {return true}
+        };
+
+        let spy = expect.spyOn(window.RDX, 'externalNavigate');
+
+        externalNavigate('http://www.google.com');
+
+        expect(spy).toHaveBeenCalled()
+    });
+
+    it('should run navigate Event if window.RDX exists', () => {
+        window.RDX = {
+            navigateEvent: () => {return true}
+        };
+
+        let spy = expect.spyOn(window.RDX, 'navigateEvent');
+
+        navigateEvent('oem', 'section1', 'tab click');
+
+        expect(spy).toHaveBeenCalled()
+    });
+
+    it('should run log error if window.RDX exists', () => {
+        window.RDX = {
+            logError: () => {return true}
+        };
+
+        let spy = expect.spyOn(window.RDX, 'logError');
+
+        logError('There was an error');
+
+        expect(spy).toHaveBeenCalled()
+    });
+
+    it('should run impression event if window.RDX exists', () => {
+        window.RDX = {
+            impressionEvent: () => {return true}
+        };
+
+        let spy = expect.spyOn(window.RDX, 'impressionEvent');
+
+        impressionEvent(true, 'oem', 'section1');
+
+        expect(spy).toHaveBeenCalled()
+    });
+});
